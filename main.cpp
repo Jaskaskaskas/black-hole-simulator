770a239eb6570b83ff0547b2cbd086223ecb4ca5
Jaskaskaskas <jaakko.posti@aalto.fi>
2026-01-07 15:40:05 +0200

    wip camera angle

--- main.cpp
+++ main.cpp
@@ -53,9 +53,14 @@ int main(int argc, char* argv[]) {
   // The origin is at the center of the window
   blackhole bh = {0.0f, 0.0f, 15.0f};
   accretiondisk ad = {bh.sradius * 1.8f, bh.sradius * 3.0f, 80.0f, 4.0f};
+  vec3 camera = {-250.0f, -50.0f, 0.0f};
+  float camera_theta = 0.0f;  // in radians in the x-z plane
+  float camera_phi = 0.0f;    // in radians in the x-y plane
 
-  float c = 1.0f;
-  float angle = 0.0f * M_PI / 180.0f;
+  const float fov = 60.0f;  // degrees
+
+  const float dangle = (fov * 3.14159265f / 180.0f) /
+                       img.width;  // vertical and horizontal fovs are the same
 
   const float X = -250.0f;
   const float Y = -150.0f;
@@ -68,21 +73,20 @@ int main(int argc, char* argv[]) {
   photons ps;
   for (int x = 0; x < img.width; x++) {
     for (int y = 0; y < img.height; y++) {
-      float startX = X;
-      float startY = Y + (y * dy);
-      float startZ = Z + (x * dz);
-
       photon p = {};
       p.idx = x;
       p.idy = y;
-      p.x = startX;
-      p.y = startY;
-      p.z = startZ;
+      p.x = camera.x;
+      p.y = camera.y;
+      p.z = camera.z;
 
       // Point them toward the Black Hole (positive X direction)
-      p.vx = 1.0f;
-      p.vy = 0.2f;
-      p.vz = 0.0f;
+      p.vx = cosf(camera_phi) * cosf(camera_theta);
+      p.vy = sinf(camera_phi);
+      p.vz = sinf(camera_theta) *
+             cosf(camera_phi);  // TODO: add the offset for each photon, has to
+                                // maybe be implemented using sort of local
+                                // coordinates?
 
       ps.list.push_back(p);
     }
